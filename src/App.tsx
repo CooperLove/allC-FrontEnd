a63c34d1961a0dbc226b4fc1ba88ac374067efa5
CooperLove <pedroleo-mn@hotmail.com>
2023-05-06 14:36:38 -0300

    style: added register user drawer - light and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../src/chakra/theme.ts";
+
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <LoginPage />
       <footer className="footer">v1.0</footer>
     </ChakraProvider>
